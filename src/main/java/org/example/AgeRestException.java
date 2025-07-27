90a83e0bec6a5011961b1530e0cfac0bf7b58739
pavel <pavelkoch7@yandex.ru>
2025-07-28 00:11:21 +0300

    Ready

---
+++ src/main/java/org/example/AgeRestException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class AgeRestException extends Exception{
+    public AgeRestExeption(String message){
+        super(message);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main (String[] args){
+        try{
+            AgeValidator.checkAge(16);
+        } catch (AgeRestException e){
+            System.out.println ("Ошибка: " + e.getMessage());
+        }
+    }
 }
